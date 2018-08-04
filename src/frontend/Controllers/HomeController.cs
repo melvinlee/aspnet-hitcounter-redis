557e0a98de71d578559955a2e94ce50dec059961
melvinlee <melvinlee@hotmail.com>
2018-08-04 12:26:58 +0800

    feat: added frontend apps.

---
+++ src/frontend/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace frontend.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var count = 0;
+            return $"You are visitor no. {count}";
+        }
+    }
+}
